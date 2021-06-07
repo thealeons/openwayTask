226daefc21ae694851417fb92d145a95e5d424ae
aleksandr.s.leonov@nw.rt.ru <aleksandr.s.leonov@nw.rt.ru>
2021-06-08 01:44:32 +0300

    + add text in comment (without logic)

--- src/main/java/ru/aleons/events/IssueCommentListener.java
+++ src/main/java/ru/aleons/events/IssueCommentListener.java
@@ -1,20 +1,33 @@
 package ru.aleons.events;
 
 import com.atlassian.event.api.EventListener;
+import com.atlassian.jira.component.ComponentAccessor;
 import com.atlassian.jira.event.issue.IssueEvent;
 import com.atlassian.jira.event.type.EventType;
-import com.atlassian.jira.issue.Issue;
 import com.atlassian.jira.issue.comments.Comment;
+import com.atlassian.jira.issue.comments.CommentManager;
+import com.atlassian.jira.issue.comments.MutableComment;
+import ru.aleons.logic.EditCommentManager;
+
 
 public class IssueCommentListener {
 
+    private final CommentManager commentManager;
+
+    public IssueCommentListener() {
+        this.commentManager = ComponentAccessor.getComponent(CommentManager.class);
+    }
 
     @EventListener
     public void onCommentEvent(IssueEvent issueEvent) {
         Long eventTypeId = issueEvent.getEventTypeId();
-        Issue issue = issueEvent.getIssue();
         if (eventTypeId.equals(EventType.ISSUE_COMMENTED_ID) || eventTypeId.equals(EventType.ISSUE_COMMENT_EDITED_ID)) {
             Comment comment = issueEvent.getComment();
+            EditCommentManager editCommentManager = new EditCommentManager();
+            String newCommentText = editCommentManager.checkBodyByLink(comment.getBody());
+            MutableComment newComment =commentManager.getMutableComment(comment.getId());
+            newComment.setBody(newCommentText);
+            commentManager.update(newComment, false);
         }
 
 
--- src/main/java/ru/aleons/logic/EditCommentManager.java
+++ src/main/java/ru/aleons/logic/EditCommentManager.java
@@ -1,6 +1,9 @@
 package ru.aleons.logic;
 
+
 public class EditCommentManager {
 
-//    public String
+    public String checkBodyByLink(String body){
+        return body + " Не могу придумать регулярку для получения нужных ссылок =(";
+    }
 }
